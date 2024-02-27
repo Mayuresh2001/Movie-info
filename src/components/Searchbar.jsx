00be9d5d010299d25f97885549403da28715e0bb
Mayuresh2001 <mayureshvejare@gmail.com>
2024-02-27 15:38:47 +0530

    2nd commit

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -21,17 +21,17 @@ function Searchbar({  updateParam ,isLoading, setIsLoading, cardHidden, setCardH
   }
 
   return (
-    <div className="flex md:flex-row flex-col  md:justify-between justify-center mx-auto w-10/12 items-center mt-10 md:-space-x-[20%]" >
+    <div className="flex md:flex-row flex-col  md:justify-center justify-center mx-auto w-10/12  mt-10 md:space-x-1" >
       <label className="m-auto w-full text-center relative" htmlFor='search'>
         <input onFocus={() =>setCardHidden(false)}
           type="text"
           placeholder="Search Any Movies & TV Shows "
-          className="searchbox text-md md:text-xl border-black border w-2/3 h-9 mx-auto md:h-11 px-4 rounded-sm "
+          className="searchbox -ml-36 text-md md:text-xl border-black border w-2/3 h-9 mx-auto md:h-11 px-4 rounded-sm "
           id="search"
           onInput={name}
         />
         {/* clear button */}
-        <button onClick={clearSearch} title="Clear" className="absolute  right-[18%]  top-[16%] md:top-[10px] ">
+        <button onClick={clearSearch} title="Clear" className="absolute right-[25%] top-[16%] md:top-[10px] ">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -49,7 +49,7 @@ function Searchbar({  updateParam ,isLoading, setIsLoading, cardHidden, setCardH
         </button>
       </label>
       <button
-        className="bg-slate-900 text-white h-9  md:h-11 md:text-2xl text-md my-4 md:my-1 px-2 rounded-sm  "
+        className="bg-slate-900 text-white h-9 md:h-11 md:text-xl text-md my-4 md:my-1 px-2 rounded-sm -translate-x-52"
         type="submit"
         onClick={()=>name()}
         >
